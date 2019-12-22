695ad1fc0462f6bb72e50bf0769a96dc831f900b
zhouwe1 <zhouwei@live.it>
2019-12-22 16:39:00 +0800

    financing模型

---
+++ webapp/models/financing_models.py
@@ -0,0 +1,60 @@
+from sqlalchemy import UniqueConstraint
+from webapp.extentions import db
+
+
+class UserAsset(db.Model):
+    """
+    用户投资项目
+    """
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(32))
+    agent_id = db.Column(db.Integer(), db.ForeignKey('agent.id', ondelete='RESTRICT'))
+    fp = db.Column(db.Integer(), db.ForeignKey('financial_product.id', ondelete='RESTRICT'))
+    start_time = db.Column(db.DateTime(timezone=True))  # 第一次买入时间
+    update_time = db.Column(db.DateTime(timezone=True))  # 最后一次买入时间
+    amount = db.Column(db.Integer())
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='RESTRICT'))
+
+
+class Agent(db.Model):
+    """购买处(银行/基金公司/支付宝/微信)"""
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(16), unique=True)
+    assets = db.relationship(
+        'UserAsset',
+        backref='agent',
+        lazy='dynamic'
+    )
+
+
+fp_assets = db.Table(
+    'fp_assets',
+    db.Column('fp_id', db.Integer(), db.ForeignKey('financial_product.id')),
+    db.Column('fpa_id', db.Integer(), db.ForeignKey('fp_asset.id')),
+    UniqueConstraint('fp_id', 'fpa_id', name='fp_fpa_unique')  # 联合唯一索引,name索引的名字
+)
+
+
+class FinancialProduct(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(32), unique=True)
+    fund_code = db.Column(db.Integer(), unique=True)  # 基金代码
+    stock_code = db.Column(db.Integer(), unique=True)  # 基金代码
+    url = db.Column(db.Text(), default='{}')
+    meta = db.Column(db.Text(), default='{}')
+    update_time = db.Column(db.DateTime(timezone=True))  # 更新时间
+    assets = db.relationship(
+        'FPAsset',
+        secondary=fp_assets,
+        backref=db.backref('fps', lazy='dynamic')
+    )
+
+
+class FPAsset(db.Model):
+    """
+    金融产品的资产分布枚举： 股票、债券、现金、其他
+    """
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(16), unique=True)
+
+
