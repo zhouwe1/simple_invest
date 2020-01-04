5bc76915b0d4e424647afefcb27cdcfcd6e90be8
zhouwe1 <zhouwei@live.it>
2020-01-04 18:08:16 +0800

    优化dashboard

--- webapp/controllers/home.py
+++ webapp/controllers/home.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
 from flask_login import login_user, logout_user, login_required, current_user
 from webapp.models.user_models import User
-from webapp.models.financing_models import UserAsset, FPType
+from webapp.models.financing_models import UserAsset, FPType, Agent
 
 
 home_blueprint = Blueprint(
@@ -81,50 +81,43 @@ def logout():
 def dashboard():
     target = 400000
     agent_dict = dict()
-    fp_type_dict = dict()
+    fptype_dict = dict()
     total_amount = 0  # 总金额
     last_update = None
     fp_count = 0
     for ua in UserAsset.query.filter_by(user_id=current_user.id, is_delete=False).all():
+        # 获取最后更新时间
         if not last_update:
             last_update = ua.update_time
-        else:
-            if ua.update_time > last_update:
-                last_update = ua.update_time
-        agent_id = ua.agent_id
-        agent_name = ua.agent.name
+        elif ua.update_time > last_update:
+            last_update = ua.update_time
+
         amount = ua.last_amount.amount_yuan
-        fp_type_id = ua.financial_product.type_id
         total_amount += amount
         fp_count += 1
+
+        # 汇总每个渠道的总金额
+        agent_id = ua.agent_id
         if agent_id in agent_dict:
-            agent_dict[agent_id]['amount'] += amount
-            agent_dict[agent_id]['count'] += 1
+            agent_dict[agent_id] += amount
         else:
-            agent_dict[agent_id] = {
-                'name': agent_name,
-                'amount': amount,
-                'count': 1,
-            }
-        for _, agent in agent_dict.items():
-            agent['amount_rate'] = '{}%'.format(round(agent['amount'] / total_amount * 100, 1))
+            agent_dict[agent_id] = amount
 
-        if fp_type_id in fp_type_dict:
-            fp_type_dict[fp_type_id]['amount'] += amount
-            fp_type_dict[fp_type_id]['count'] += 1
+        # 汇总每个理财类型的总金额
+        fp_type_id = ua.financial_product.type_id
+        if fp_type_id in fptype_dict:
+            fptype_dict[fp_type_id] += amount
         else:
-            fp_type_dict[fp_type_id] = {
-                'amount': amount,
-                'name': FPType.dict().get(fp_type_id),
-                'count': 1,
-            }
-        for _, ft_t in fp_type_dict.items():
-            ft_t['amount_rate'] = '{}%'.format(round(ft_t['amount'] / total_amount * 100, 1))
+            fptype_dict[fp_type_id] = amount
 
-    agent_list = list(agent_dict.values())
-    agent_list.sort(key=lambda x: x.get('amount'), reverse=True)
-    fpt_list = list(fp_type_dict.values())
-    fpt_list.sort(key=lambda x: x.get('amount'), reverse=True)
+    agent_tuples = []
+    for k, v in agent_dict.items():
+        agent_tuples.append((Agent.name_cache().get(k), v))
+    agent_tuples.sort(key=lambda x: x[1], reverse=True)
+    fptype_tuples = []
+    for k, v in fptype_dict.items():
+        fptype_tuples.append((FPType.dict().get(k), v))
+    fptype_tuples.sort(key=lambda x: x[1], reverse=True)
     return render_template(
         'home/dashboard.html',
         target=400000,
@@ -132,6 +125,6 @@ def dashboard():
         fp_count=fp_count,
         total_amount=total_amount,
         last_update=last_update,
-        agent_list=agent_list,
-        fpt_list=fpt_list,
+        agent_tuples=agent_tuples,
+        fptype_tuples=fptype_tuples
     )
