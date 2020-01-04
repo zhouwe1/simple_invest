f2f202850ff5045104d83129e49e893368285cf8
zhouwe1 <zhouwei@live.it>
2020-01-05 01:02:45 +0800

    提供一个类似todo清单的更新方式，更新后记录被隐藏，只留下待更新的记录

--- webapp/static/js/tools.js
+++ webapp/static/js/tools.js
@@ -3,35 +3,61 @@ $(function () {
     load_avatar(user_avatar);
 
     $('.tr-clone').click(function () {
-    let clone_tr = $('#dataTbody .clone-tr');
+        let clone_tr = $('#dataTbody .clone-tr');
 
-    if (clone_tr.length > 0){
-        clone_tr.addClass('tr-error');
-        setTimeout(function(){
-            clone_tr.removeClass('tr-error')
-        }, 100)
-    }
-    else{
-        $('#dataTbody').prepend($('#cloneTbody > tr').clone())
-    }
-});
+        if (clone_tr.length > 0){
+            clone_tr.addClass('tr-error');
+            setTimeout(function(){
+                clone_tr.removeClass('tr-error')
+            }, 100)
+        }
+        else{
+            $('#dataTbody').prepend($('#cloneTbody > tr').clone())
+        }
+    });
 
-$('.tr-add').click(function () {
-    let newTbody = $('#newTbody');
-    if(newTbody.hasClass('d-none')){
-        newTbody.removeClass('d-none');
-        newTbody.find('input:first').focus()
-    }
-    else{
-        newTbody.children('tr').addClass('tr-error');
-        setTimeout(function(){
-            newTbody.children('tr').removeClass('tr-error')
-        }, 100);
-        newTbody.find('input:first').focus()
-    }
-});
+    $('.tr-add').click(function () {
+        let newTbody = $('#newTbody');
+        if(newTbody.hasClass('d-none')){
+            newTbody.removeClass('d-none');
+            newTbody.find('input:first').focus()
+        }
+        else{
+            newTbody.children('tr').addClass('tr-error');
+            setTimeout(function(){
+                newTbody.children('tr').removeClass('tr-error')
+            }, 100);
+            newTbody.find('input:first').focus()
+        }
+    });
 
+    $('.tr-todo').click(function () {
+        let todo_mode = $('#dataTbody').attr('data-todo');
+        if(todo_mode=='on'){
+            $.growl.notice({title: '已退出Todo模式', message: '请等待页面刷新', location:"tr"});
+            setTimeout(function(){
+                location.reload()
+            }, 1000);
+        }
+        else{
+            let today = moment().format("YYYY-MM-DD");
+            $('#dataTbody').attr('data-todo', 'on');
+            $(this).addClass('btn-primary');
+            $(this).removeClass('btn-default');
+            $.growl.notice({title: 'Todo模式开启', message: '今天内更新过的记录都会被隐藏，只留下待更新的记录', location:"tr"});
+            $('#dataTbody tr').each(function () {
+                let tr_date = $(this).attr('data-update_time'),
+                    tr = $(this);
+                if(moment(tr_date).isAfter(today)){
+                    tr.addClass('tr-success');
+                    setInterval(function () {
+                        tr.remove()
+                    }, 1000);
+                }
+            })
+        }
 
+    })
 });
 
 
