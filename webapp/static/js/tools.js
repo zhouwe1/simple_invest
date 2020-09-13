c8e1094939caa266088ae8465a39fa649239b76c
zhouwe1 <zhouwei@live.it>
2020-09-13 15:11:59 +0800

    优化line图的显示

--- webapp/static/js/tools.js
+++ webapp/static/js/tools.js
@@ -153,11 +153,8 @@ function fill_trend_amount_chart(url) {
                     {
                         backgroundColor     : 'rgba(60,141,188,0.9)',
                         borderColor         : 'rgba(60,141,188,0.8)',
-                        pointRadius          : false,
-                        pointColor          : '#3b8bba',
-                        pointStrokeColor    : 'rgba(60,141,188,1)',
-                        pointHighlightFill  : '#fff',
-                        pointHighlightStroke: 'rgba(60,141,188,1)',
+                        pointBackgroundColor: "rgb(197,219,241)",
+                        pointBorderColor    : "rgb(1,84,114)",
                         data                : r['datas']
                     },
                 ]
@@ -181,11 +178,8 @@ function fill_trend_ua_chart(labels, datas) {
                 {
                     backgroundColor     : 'rgba(60,141,188,0.9)',
                     borderColor         : 'rgba(60,141,188,0.8)',
-                    pointRadius          : false,
-                    pointColor          : '#3b8bba',
-                    pointStrokeColor    : 'rgba(60,141,188,1)',
-                    pointHighlightFill  : '#fff',
-                    pointHighlightStroke: 'rgba(60,141,188,1)',
+                    pointBackgroundColor: "rgb(197,219,241)",
+                    pointBorderColor    : "rgb(1,84,114)",
                     data                : datas
                 },
             ]
