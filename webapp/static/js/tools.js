63e024069ce9188d7511b5341e5e2fccbd54575a
zhouwei <zhouwei@live.it>
2023-07-31 10:44:15 +0800

    优化js代码

--- webapp/static/js/tools.js
+++ webapp/static/js/tools.js
@@ -1,44 +1,53 @@
-$(function () {
+let submit_btn_val='<i class="fa fa-check"></i>',
+    update_btn_val='<i class="fas fa-pencil-alt"></i>',
+    cancel_btn_val='<i class="fa fa-undo"></i>',
+    delete_btn_val='<i class="fas fa-trash"></i>',
+    b_head = '<button type="button" class="btn ',
+    b_tail = '</button> ',
+    submit_btn_f_html=b_head+'btn-success tr-submit d-none">'+submit_btn_val+b_tail,
+    update_btn_t_html=b_head+'btn-info tr-update">'+update_btn_val+b_tail,
+    cancel_btn_f_html=b_head+'btn-warning tr-cancel d-none">'+cancel_btn_val+b_tail,
+    delete_btn_t_html=b_head+'btn-danger tr-delete">'+delete_btn_val+b_tail;
+$(function(){
     refresh_sidebar();
     load_avatar(user_avatar);
 
-    $('.tr-clone').click(function () {
-        let clone_tr = $('#dataTbody .clone-tr');
+    $('.tr-clone').click(function(){
+        let clone_tr=$('#dataTbody .clone-tr');
         if (clone_tr.length > 0){
             clone_tr.addClass('tr-error');
             setTimeout(function(){clone_tr.removeClass('tr-error')},100)
-        }
-        else{$('#dataTbody').prepend($('#cloneTbody > tr').clone())}
+        }else{$('#dataTbody').prepend($('#cloneTbody>tr').clone())}
     });
 
-    $('.tr-add').click(function () {
-        let newTbody = $('#newTbody');
+    $('.tr-add').click(function(){
+        let newTbody=$('#newTbody');
         if(newTbody.hasClass('d-none')){
             newTbody.removeClass('d-none');
             newTbody.find('input:first').focus()
         }
         else{
             newTbody.children('tr').addClass('tr-error');
-            setTimeout(function(){newTbody.children('tr').removeClass('tr-error')}, 100);
+            setTimeout(function(){newTbody.children('tr').removeClass('tr-error')},100);
             newTbody.find('input:first').focus()
         }
     });
 
-    $('.tr-todo').click(function () {
+    $('.tr-todo').click(function() {
         let todo_mode = $('#dataTbody').attr('data-todo');
         if(todo_mode=='on'){
             swalSuccess('已退出Todo模式，请等待页面刷新');
             setTimeout(function(){location.reload()},1000);
         }
         else{
-            let today = moment().format("YYYY-MM-DD");
-            $('#dataTbody').attr('data-todo', 'on');
+            let today=moment().format("YYYY-MM-DD");
+            $('#dataTbody').attr('data-todo','on');
             $(this).addClass('btn-primary');
             $(this).removeClass('btn-default');
             swalInfo('Todo模式开启，今天内更新过的记录都会被隐藏，只留下待更新的记录');
-            $('#dataTbody tr').each(function () {
-                let tr_date = $(this).attr('data-update_time'),
-                    tr = $(this);
+            $('#dataTbody tr').each(function(){
+                let tr=$(this),
+                    tr_date=tr.attr('data-update_time');
                 if(moment(tr_date).isAfter(today)){
                     tr.addClass('tr-success');
                     trRemove(tr)
@@ -47,46 +56,44 @@ $(function () {
         }
     });
 
-    $('.dashboard-pie-view-detail').click(function () {
-        window.location.href='/analyse/scale'
-    })
+    $('.dashboard-pie-view-detail').click(function(){window.location.href='/analyse/scale'})
 });
 
-let Toast = Swal.mixin({
-      toast: true,
-      position: 'top-end',
-      showConfirmButton: false,
+let Toast=Swal.mixin({
+      toast:true,
+      position:'top-end',
+      showConfirmButton:false,
       timer: 3000
     });
 
 let pieOptions = {
-        maintainAspectRatio : false,
-        responsive : true,
-        legend: {position: 'right'}
+        maintainAspectRatio:false,
+        responsive:true,
+        legend: {position:'right'}
     },
     areaChartOptions = {
-        maintainAspectRatio : false,
+        maintainAspectRatio:false,
         responsive : true,
-        legend: {display: false},
+        legend: {display:false},
         scales: {
-            xAxes: [{
-                gridLines : {display : false}
+            xAxes:[{
+                gridLines:{display:false}
             }],
-            yAxes: [{
-                gridLines : {display : false}
+            yAxes:[{
+                gridLines:{display:false}
             }]
         }
     };
 
-function load_avatar(url) {
+function load_avatar(url){
     $('#nav-user-avatar').prepend('<img alt src="'+ url +'" class="user-image img-circle elevation-2">');
     $('.user-header').prepend('<img src="'+ url +'" class="img-circle elevation-2" alt="User Image">')
 }
 
-function refresh_sidebar() {
-    let current_path = window.location.pathname;
-    $('.nav-sidebar a').each(function () {
-        if(current_path==$(this).attr('href')){
+function refresh_sidebar(){
+    let current_path=window.location.pathname;
+    $('.nav-sidebar a').each(function(){
+        if(current_path===$(this).attr('href')){
             $(this).parents('li').children('a').addClass('active');
             $(this).parents('li.has-treeview').addClass('menu-open')
         }
@@ -127,7 +134,7 @@ function fill_dashboard_fptype_pie(labels, datas){
     });
 }
 
-function fill_trend_amount_chart(url) {
+function fill_trend_amount_chart(url){
     //总额趋势图
     $.ajax({
         url: url,
@@ -166,7 +173,7 @@ function fill_trend_amount_chart(url) {
     })
 }
 
-function fill_trend_ua_chart(labels, datas) {
+function fill_trend_ua_chart(labels, datas){
     //持仓理财产品趋势图
     $("html,body").animate({scrollTop:$("#uaTrend").offset().top},1000);
     let areaChartCanvas = document.getElementById("uaTrend").getContext('2d'),
@@ -190,30 +197,26 @@ function fill_trend_ua_chart(labels, datas) {
     })
 }
 
-function swalError(msg) {Toast.fire({type: 'error', title: msg})}
-function swalInfo(msg) {Toast.fire({type: 'info', title: msg})}
-function swalSuccess(msg) {Toast.fire({type: 'success', title: msg})}
+function swalError(msg){Toast.fire({type:'error',title:msg})}
+function swalInfo(msg){Toast.fire({type:'info',title:msg})}
+function swalSuccess(msg){Toast.fire({type:'success',title: msg})}
 
 
-function trError(tr, msg=''){
+function trError(tr,msg=''){
     tr.addClass('tr-error');
-    setInterval(function (){tr.removeClass('tr-error')}, 1000);
-    if (msg){swalError(msg)}
+    setInterval(function(){tr.removeClass('tr-error')},1000);
+    if(msg){swalError(msg)}
 }
 
-function trSuccess(tr){
-    tr.addClass('tr-success');
-    setInterval(function (){tr.removeClass('tr-success')}, 1000);
-}
-
-function trRemove(tr){setInterval(function(){tr.remove()},1000);}
+function trSuccess(tr){tr.addClass('tr-success');setInterval(function(){tr.removeClass('tr-success')},1000)}
+function trRemove(tr){setInterval(function(){tr.remove()},1000)}
 
 function disableBtn(btn){
-    btn.html('<i class="fa fa-spinner fa-pulse"></>');  // 把按钮改成loading图标
-    btn.attr('disabled', 'disabled');  // 禁用按钮
+    btn.html('<i class="fa fa-spinner fa-pulse"></>');//把按钮改成loading图标
+    btn.attr('disabled','disabled');
 }
 
-function resumeBtn(btn, action='submit'){
+function resumeBtn(btn, action){
      // 把按钮从loading图标恢复成原有样式
     btn.removeAttr('disabled');
     if (action==='submit'||action==='s'){btn.html('<i class="fa fa-check"></i>')}
@@ -222,11 +225,11 @@ function resumeBtn(btn, action='submit'){
     else if(action==='cancel'||action==='c'){btn.html('<i class="fa fa-undo"></i>')}
 }
 
-
-function getSubmitBtn(tr){return tr.find('.tr-submit')}
-function getUpdateBtn(tr){return tr.find('.tr-update')}
-function getCancelBtn(tr){return tr.find('.tr-cancel')}
-function getDeleteBtn(tr){return tr.find('.tr-delete')}
+function getBtn(tr, name){return tr.find('.tr-'+name)}
+function getBtns(tr){return [tr.find('.tr-submit'),tr.find('.tr-update'),tr.find('.tr-cancel'),tr.find('.tr-delete')]}
 
 function hideBtns(buttons){for(let index in buttons){buttons[index].addClass('d-none')}}
 function showBtns(buttons){for(let index in buttons){buttons[index].removeClass('d-none')}}
+
+function agentFilter(){window.location.href='?agent='+$('#agent_filter').val()}
+function typeFilter(){window.location.href='?fp_type='+$('#type_filter').val()}
