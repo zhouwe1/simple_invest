9fdf57949af3aa89fd174cc11e09a7e331a87cbe
zhouwei <zhouwei@live.it>
2024-05-13 23:14:26 +0800

    提交基础信息

---
+++ run.sh
@@ -0,0 +1,7 @@
+cd /var/www/financing/
+git pull
+source /var/venv/financing/bin/activate
+export FLASK_APP=run.py
+#pip install -r requirements
+flask db upgrade
+exec gunicorn -w 1 --threads=2 -b 127.0.0.1:8001 webapp:flask_app &
