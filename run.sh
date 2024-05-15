4944ec5accdcbf31c4afdf7e2d5c7f30b448f46e
zhouwei <zhouwei@live.it>
2024-05-15 15:05:22 +0800

    修复持仓金额不能改为0的错误

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ cd /var/www/financing/
 git pull
 source /var/venv/financing/bin/activate
 export FLASK_APP=run.py
-#pip install -r requirements
+#pip install -r requirements.txt
 flask db upgrade
 exec gunicorn -w 1 --threads=2 -b 127.0.0.1:8001 webapp:flask_app &
